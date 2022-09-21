1628b896738200e9e5c712392fde2a967ad1016c
rayhanbintang <raybintangabd@gmail.com>
2022-09-21 11:25:29 +0700

    00 planned

--- modules/01_prereq/vpc.tf
+++ modules/01_prereq/vpc.tf
@@ -4,14 +4,3 @@ resource "aws_vpc" "main-vpc" {
 	# checkov:skip=BC_AWS_LOGGING_9: ADD REASON
   cidr_block = "10.0.0.0/16"
 }
-
-resource "aws_subnet" "main-subnet" {
-    #TODO: UPDATE WITH VAR INSTEAD OF HARDCODED
-  vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "use1-az1"
-
-  tags = {
-    Name = "Main"
-  }
-}
